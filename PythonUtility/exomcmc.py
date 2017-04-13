f381cd0cc0d089d7450308710795e2376f5c4056
Walter Silva Martins Filho <walter@on.br>
2017-04-13 02:52:20 -0300

    MCMC for ICA routine

--- PythonUtility/exomcmc.py
+++ PythonUtility/exomcmc.py
@@ -1,53 +1,184 @@
 import numpy as np
+from pandas import DataFrame
+import emcee
 
+def exomcmc(hjd,rawflux,eflux,pfit,sigma,ndim,nwalkers,Nsize,a_Rs,inc,limbB1,limbB2,period,e,omega,show=True,space=1e-4,lnf0=np.log(0.43)):
+    '''
+    Executing the Monte Carlo Markov Chain in Exoplanetary Transits
+    ---
+    INPUT:
+    pfit:
+    ndim
+    nwalkers
+    lnprob: A function that takes a vector in the parameter space as input and
+            returns the natural logarithm of the posterior probability for that
+            position
+    show:   True or False. Shows in terminal the output in each step.
+    space:  the size of the possible random value pick in the distribution
+            on the space parameter. This give to you how much the random walk
+            will be far to relative in the initial value gived.
+    Nsize:  number of total chains in the MCMC Algoritm.
+    '''
 
-def lnlike(theta,x,y,yerr):
-    """
-    THis function returns the maximum likelihood following the ideas of Hoog et al.(2010) 
-    to fitting a model to data using Markov Chain Monte Carlo Method using Metropole-Hastings Algoritm and Gibbs Sample.
-    ___
-    INPUT
-    
-    theta: turple with the parameters to fit and the confindence interval of fractional size f
-    x, y: data to fit
-    yerr: error of our data
-    
-    OUTPUT:
-    
-    natural logaritm of the likelihood function
-    """
-    p1, p2, p3, lnf = theta
-    model = model_am_exp(x,p1, p2, p3, 0)
-    inv_sigma2 = 1.0/(yerr**2 + model**2*np.exp(2*lnf))
-    return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
+    def lnlike(theta,x,y,yerr):
+        import lightcurve #Kyle's lightcurve code
+        import lightcurveMCMC
+        """
+        THis function returns the maximum likelihood following the ideas of Hoog et al.(2010)
+        to fitting a model to data using Markov Chain Monte Carlo Method using Metropole-Hastings Algoritm and Gibbs Sample.
+        ___
+        INPUT
 
-def lnprior(theta):
-    """
-    This function is where our sicence prior is established.
-    ___
-    INPUT
-    
-    theta: turple with the parameters to fit and the confindence interval of fractional size f
-    
-    OUTPUT
-    
-    The cts of the with prior information
-    """
-    p1, p2, p3, lnf = theta
-    
-    minRp, maxRp = pfit[0] - 7.0*sigma[0], pfit[0] + 7.0*sigma[0]
-    minTc, maxTc = pfit[1] - 7.5*sigma[1], pfit[1] + 7.5*sigma[1]
-    minA, maxA = pfit[2] - 7.5*sigma[2], pfit[2] + 7.5*sigma[2]
-    
-    if minRp < p1 < maxRp and  minTc < p2 < maxTc and minA < p3 < maxA and -10.0 < lnf < 1.0:
-        return 0.0
-    return -np.inf
+        theta: turple with the parameters to fit and the confindence interval of fractional size f
+        x, y: data to fit
+        yerr: error of our data
 
-def lnprob(theta, x, y, yerr):
-    lp = lnprior(theta)
-    if not np.isfinite(lp):
+        OUTPUT:
+
+        natural logaritm of the likelihood function
+        """
+        p1, p2, p3, lnf = theta
+
+        # model = model_am_exp(x,p1, p2, p3, 0)
+        fit_result_lnlike = lightcurveMCMC.lightcurve_fit(float(p1),np.mean(x),a_Rs,inc,limbB1,limbB2,period,e,omega,np.array(x),np.array(y),np.array(yerr))
+        model_standart = (fit_result_lnlike.final_curve - np.mean(fit_result_lnlike.final_curve))/np.std(fit_result_lnlike.final_curve)
+        model = model_standart * np.std(y) + np.mean(y)
+
+        inv_sigma2 = 1.0/(yerr**2 + model**2*np.exp(2*lnf))
+        return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
+
+    def lnprior(theta):
+        """
+        This function is where our sicence prior is established.
+        ___
+        INPUT
+
+        theta: turple with the parameters to fit and the confindence interval of fractional size f
+
+        OUTPUT
+
+        The cts of the with prior information
+        """
+        p1, p2, p3, lnf = theta
+
+        minRp, maxRp = pfit[0] - 7.0*sigma[0], pfit[0] + 7.0*sigma[0]
+        minTc, maxTc = pfit[1] - 7.5*sigma[1], pfit[1] + 7.5*sigma[1]
+        minA, maxA = pfit[2] - 7.5*sigma[2], pfit[2] + 7.5*sigma[2]
+
+        if minRp < p1 < maxRp and  minTc < p2 < maxTc and minA < p3 < maxA and -10.0 < lnf < 1.0:
+            return 0.0
         return -np.inf
-    return lp + lnlike(theta, x, y, yerr)
 
-def mcmc():
-    
+    def lnprob(theta, x, y, yerr):
+        lp = lnprior(theta)
+        if not np.isfinite(lp):
+            return -np.inf
+        return lp + lnlike(theta, x, y, yerr)
+
+    ############################################################################
+    ############################################################################
+    hjd,rawflux,eflux= np.array(hjd),np.array(rawflux),np.array(eflux)
+    #defining the veriables
+    p1, p2, p3 = pfit
+    s1, s2, s3 = sigma
+    lnf = lnf0
+    minTc, maxTc = p2 - 2.5* s2, p2 + 2.5*s2
+    if show == True:
+        print(minTc, maxTc)
+    startpar = [p1, p2, p3,lnf]
+    if show == True:
+        print('Initial parameters = ',startpar)
+        print('Initial Dispersion of the initial parameters = ',sigma)
+    #ndim, nwalkers = 4, 8
+    pos = [startpar + space*np.random.randn(ndim) for i in range(nwalkers)]
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(hjd, rawflux, eflux))
+    sampler.run_mcmc(pos, Nsize)
+    if show == True:
+        print('Acceptance fraction = ',np.mean(sampler.acceptance_fraction))
+    #Create the dataframe OUTPUT
+    #samples = sampler.chain[:, 50:, :].reshape((-1, ndim)) #test with only the first 50 chains
+    data = DataFrame(sampler.chain.reshape((-1, ndim)))
+    data.columns = ['RpRs','Tc','A','lnf']
+    if show == True:
+        print('The percentiles for Rp/Rs are: ')
+        print('31% Rp/Rs = ',np.percentile(data.RpRs,31),' and the inferior error are = ', abs(np.percentile(data.RpRs,50)-np.percentile(data.RpRs,31)))
+        print('50% Rp/Rs = ',np.percentile(data.RpRs,50))
+        print('81% Rp/Rs = ',np.percentile(data.RpRs,81),' and the superior error are = ', abs(np.percentile(data.RpRs,50)-np.percentile(data.RpRs,81)))
+
+        print('The percentiles for Tc are: ')
+        print('31% Tc = ',np.percentile(data.Tc,31),' and the inferior error are = ', abs(np.percentile(data.Tc,50)-np.percentile(data.Tc,31)))
+        print '50% Tc = ',np.percentile(data.Tc,50)
+        print '81% Tc = ',np.percentile(data.Tc,81),' and the superior error are = ', abs(np.percentile(data.Tc,50)-np.percentile(data.Tc,81))
+
+        print('The percentiles for A are: ')
+        print('31% A = ',np.percentile(data.A,31),' and the inferior error are = ', abs(np.percentile(data.A,50)-np.percentile(data.A,31)))
+        print '50% A = ',np.percentile(data.A,50)
+        print '81% A = ',np.percentile(data.A,81),' and the superior error are = ', abs(np.percentile(data.A,50)-np.percentile(data.A,81))
+
+        print('The percentiles for lnf are: ')
+        print('31% lnf = ',np.percentile(data.lnf,31),' and the inferior error are = ', abs(np.percentile(data.lnf,50)-np.percentile(data.lnf,31)))
+        print '50% lnf = ',np.percentile(data.lnf,50)
+        print '81% lnf = ',np.percentile(data.lnf,81),' and the superior error are = ', abs(np.percentile(data.lnf,50)-np.percentile(data.lnf,81))
+
+    return data
+
+def mcmc(hjd,rawflux,eflux,pfit,sigma,N):
+    '''
+    Metropolis-Hastings MCMC
+    '''
+    def chisquare_dof(x,y,eps,pfit):
+        """
+        Return the chisquare data.
+
+        """
+        a,b,c = pfit
+        residuos = (y - model_am_exp(hjd,a,b,c,0))/eps
+        chi2 = sum(residuos**2)
+        return chi2,residuos
+
+    def pick_mcmc(startpar):
+        pick = np.zeros(len(startpar))
+        indx = range(len(startpar))
+        pick[int(np.random.choice(indx))] = 1.
+        return pick
+
+    @jit
+    def mcmc(x,y,eps,sigma,pfit,togsig,N):
+        param = np.zeros(len(pfit))
+        param = pfit + np.random.normal(size=len(pfit)) * sigma * togsig
+        #if param[0] < 0: #thi is a force in our mcmc routine to maintenece the first parameter inside the phase space
+        #    while param[0] < 0:
+        #        param = pfit + np.random.normal(size=len(pfit)) * sigma * togsig
+        chi2, residuos = chisquare_dof(x,y,eps,param)
+
+        result = []
+        chi2_result = []
+        accept = 0
+        for i in range(int(N)):
+            pick = pick_mcmc(pfit)
+            dparam = np.random.choice(pick)* np.random.normal(size=len(pfit)) * sigma *togsig
+            param_new = param + dparam
+            chi2new, residuos = chisquare_dof(x,y,eps,param_new)
+            prob = np.exp(0.5*(chi2-chi2new))
+            a = min([prob,1])
+            u = np.random.random()
+            if u <=a:
+                accept = accept + 1
+                result.append(param_new)
+                chi2_result.append(chi2new)
+                param = param_new
+                chi2 = chi2new
+        return result,chi2_result,accept/N
+    ##
+    chi2, residuos = chisquare_dof(hjd,rawflux,eflux,pfit)
+
+    if chi2/(len(rawflux)-3.) > 1:
+        print 'Your chi-squared value is more than 1. Inflating error bars.'
+        eflux = eflux * np.sqrt(chi2/(len(rawflux)-3.))
+
+    if show == True:
+        print 'Input data: '
+        print 'Iter = ',N
+        print 'Start parameters: ',np.array(pfit)
+        print 'errors: ', np.array(sigma)
+    Result,chi2result,rate = mcmc(hjd,rawflux,eflux,sigma,pfit,[3.,2.5,3.5],N)
---
+++ PythonUtility/lightcurveMCMC.py
@@ -0,0 +1,235 @@
+def lightcurve_fit(_RpRs,_tmid,_aR,_i,_u1,_u2,_P,_e,_omega,_time,_flux,_eflux):
+	'''
+	Use lmfit package
+
+	INPUT:
+		_RpRs
+	    _tmid: mid transit guess
+	    _aR:
+	    _i: inclination in degrees
+	    _u1:
+	    _u2:
+	    _P:
+	    _e:
+	    _omega:
+	    _time:
+	    _flux:
+	    _eflux:
+	'''
+	import numpy as np
+	import ctypes
+	import matplotlib.pyplot as plt
+	from lmfit import minimize as lminimize
+	from lmfit import Parameters, Parameter, report_fit
+	from os import environ
+
+	# LOAD IN C OCCULT FUNCTION
+	array_1d_double = np.ctypeslib.ndpointer(dtype=ctypes.c_double,ndim=1,flags=['C_CONTIGUOUS','aligned'])
+
+	# load library
+	lib_test = np.ctypeslib.load_library("lib_transit.so",environ['UTILPATH'])
+
+	# load fn from library and define inputs
+	occultquadC = lib_test.occultquad
+	occultquadC.argtypes = [array_1d_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, array_1d_double]
+	occultquadC.restype = None
+
+
+
+
+	######################################################################
+	#             BASIC TRANSIT FITTER (Rp,Tmid,A0,A1,A2) only
+	######################################################################
+	class param:
+	    rp = 0
+	    ar = 1
+	    P = 2
+	    i = 3
+	    u1 = 4
+	    u2 = 5
+	    e = 6
+	    w = 7
+	    tmid = 8
+
+
+	class lc_fitter(object):
+
+	    def __init__(self,t,data,dataerr=None,init=None,bounds=None,errortype='chi2',airmass=None):
+
+	        self.t = np.array(t)
+	        self.y = np.array(data)
+
+	        self.p_init = init
+	        self.errortype = errortype
+	        self.bounds = bounds
+	        self.airmass = airmass
+
+	        if type(dataerr) == type(None):
+	            self.yerr = np.ones(len(t))
+	        else:
+	            self.yerr = dataerr
+
+	        self.fit_lm()
+
+
+	    def plot_results(self):
+
+	        f,ax = plt.subplots(1)
+
+	        ax.errorbar(self.t, self.y/self.amcurve,yerr=self.yerr,fmt='ko', alpha=.2, label="f + noise")
+	        ax.plot(self.t, self.final_curve ,'--', c='r', lw=2., label="fitted f")
+
+	        ax.set_xlabel("t")
+	        ax.set_ylabel("f(t)")
+	        ax.legend(loc="best")
+	        #pl.xlim([-xlim*1.01,xlim*1.01])
+
+	        plt.show()
+
+
+	    def fit_lm(self):
+	        # use Levenberg Mardquart method
+
+
+	         # define objective function: returns the array to be minimized
+	        def fcn2min(params, x, y, yerr):
+
+	            n = len(x)
+	            model = np.zeros(n,dtype=ctypes.c_double)
+	            model = np.require(model,dtype=ctypes.c_double,requirements='C')
+
+	            occultquadC( x,params['RpRs'].value,params['aRs'].value, params['period'].value, params['inc'].value,
+	                        params['gamma1'].value, params['gamma2'].value, params['ecc'].value, params['omega'].value,
+	                        params['tmid'].value, n, model )
+
+	            model *= (params['a0'] + x*params['a1'] + x*x*params['a2'])
+
+	            return (model - y)/yerr
+
+	        #Rp,aR,P,i,u1,u2,e,omega,tmid,a0,a1,a2 = self.p_init
+	        v = [ (i[0] != i[1]) for i in self.bounds ] # boolean array to vary parameters
+	        pnames = ['RpRs','aRs','period','inc','gamma1','gamma2','ecc','omega','tmid','a0','a1','a2']
+	        params = Parameters()
+
+	        for j in range(len(self.p_init)):
+
+	            # algorithm does not like distance between min and max to be zero
+	            if v[j] == True:
+	                params.add(pnames[j], value= self.p_init[j], vary=v[j], min=self.bounds[j][0], max=self.bounds[j][1] )
+	            else:
+	                if (self.bounds[j][0] == None):
+
+	                    if (self.bounds[j][1] == None): # no upper bound
+	                        params.add(pnames[j], value= self.p_init[j], vary=True )
+	                    else: # upper bound
+	                        params.add(pnames[j], value= self.p_init[j], vary=True,max = self.bounds[j][1] )
+
+	                elif (self.bounds[j][1] == None):
+
+	                    if (self.bounds[j][0] == None): # no lower bound
+	                        params.add(pnames[j], value= self.p_init[j], vary=True )
+	                    else: # lower bound
+	                        params.add(pnames[j], value= self.p_init[j], vary=True,min = self.bounds[j][0] )
+
+	                else:
+	                    params.add(pnames[j], value= self.p_init[j], vary=v[j] )
+
+
+	         # do fit, here with leastsq model
+	        result = lminimize(fcn2min, params, args=(self.t,self.y,self.yerr))
+
+
+	        params = result.params
+	        n = len(self.t)
+	        model = np.zeros(n,dtype=ctypes.c_double)
+	        model = np.require(model,dtype=ctypes.c_double,requirements='C')
+	        occultquadC( self.t,params['RpRs'].value,params['aRs'].value, params['period'].value, params['inc'].value,
+	                    params['gamma1'].value, params['gamma2'].value, params['ecc'].value, params['omega'].value,
+	                    params['tmid'].value, n, model )
+
+	        self.final_model = model
+	        self.residuals = result.residual
+	        self.params = result.params
+	        self.result = result
+
+
+	        A0 = params['a0'].value
+	        A1 = params['a1'].value
+	        A2 = params['a2'].value
+	        self.amcurve = A0 + self.t*A1 + self.t*self.t*A2
+	        self.final_curve = self.final_model/self.amcurve
+	        self.phase = (self.t-params['tmid'].value)/params['period']
+
+
+	        # write error report
+	        # report_fit(params)
+
+	# if __name__ == "__main__":
+
+	    # if running outside folder:
+	    # from util import lc_fitter
+
+	    ############################################################
+	    ######################## Example  ##########################
+	    # generate noisy data with known coefficients
+	    # Rp = 0.055
+	    # tmid = 0.9721 # mid transit guess
+	    # aR = 14.07
+	    # i = 88.75
+	    # u1 = 0
+	    # u2 = 0.5029
+	    # P = 3.336817
+	    # e = 0
+	    # omega = 0
+	    # a0 = 1
+	    # a1 = np.random.normal(0.,0.001)
+	    # a2 = np.random.normal(0.,0.001)
+	    # t = np.linspace(0.7,1.2,200)
+
+
+	    # n = len(t)
+	    # t = np.require(t,dtype=ctypes.c_double,requirements='C')
+	    # curve = np.zeros(n,dtype=ctypes.c_double)
+	    # occultquadC(t,Rp,aR,P,i,u1,u2,e,omega,tmid,n,curve) # old results
+	    # curve = curve * (a0 + a1*t + a2*t*t)
+	    # data = curve + np.random.normal(0, 5e-4, len(curve))
+	    # dataerr = abs( 5e-4+np.random.normal(0,5e-4,len(curve)) )
+	##############################################################
+	##############################################################
+	######################## Input data  #########################
+	t 		= _time
+	Rp 		= _RpRs
+	tmid 	= _tmid
+	aR 		= _aR
+	i 		= _i
+	u1 		= _u1
+	u2 		= _u2
+	P 		= _P
+	e 		= _e
+	omega 	= _omega
+	data 	= _flux
+	dataerr = _eflux
+	##############################################################
+	######################## Fitting  ############################
+
+	p_init = [Rp,aR,P,i,u1,u2,e,omega,tmid,1,0,0]
+	mybounds = [ (0.0,1), # Rp/Rs
+	            (aR,aR), # aR
+	            (P,P), # P
+	            (i,i), # i
+	            (u1,u1), # u1
+	            (u2,u2), # u2
+	            (e,e), # e
+	            (omega,omega), # omega
+	            (min(t),max(t)), # tmid
+	            (None,None), # A0  #cooeficient linear for airmass correction
+	            (0,0), # A1 #coefficient quadratic for aimass correction
+	            (0,0)] # A2 #coefficient polynomial for airmass correction
+
+
+	myfit = lc_fitter(t,data,dataerr=dataerr,init= p_init,bounds= mybounds,)
+	# print(myfit.residuals)
+	# myfit.plot_results()
+	# report_fit(myfit.params)
+	# print('my best Rp/Rs =',myfit.params['RpRs'],'+-',myfit.params['RpRs'].stderr)
+	return myfit#.params,model
