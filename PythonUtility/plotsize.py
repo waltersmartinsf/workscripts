affe86070e6095ac0004b28536e5dc89ef1168d4
Walter Martins-Filho <waltersmartinsf@gmail.com>
2018-06-16 21:16:21 -0300

    update workscripts

---
+++ PythonUtility/plotsize.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def init_plotting(x=14,y=9):
+    plt.rcParams['figure.figsize'] = (x,y)
+    plt.rcParams['font.size'] = 20
+    #plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['axes.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['axes.titlesize'] = 0.75*plt.rcParams['font.size']
+    plt.rcParams['legend.fontsize'] = 0.65*plt.rcParams['font.size']
+    plt.rcParams['xtick.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['ytick.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['xtick.major.size'] = 3
+    plt.rcParams['xtick.minor.size'] = 3
+    plt.rcParams['xtick.major.width'] = 1
+    plt.rcParams['xtick.minor.width'] = 1
+    plt.rcParams['ytick.major.size'] = 3
+    plt.rcParams['ytick.minor.size'] = 3
+    plt.rcParams['ytick.major.width'] = 1
+    plt.rcParams['ytick.minor.width'] = 1
+    plt.rcParams['legend.frameon'] = True
+    plt.rcParams['legend.loc'] = 'best'
+    plt.rcParams['axes.linewidth'] = 1
+
+init_plotting()
